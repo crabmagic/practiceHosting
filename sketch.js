3e316272a145234c92bed39d794d5286ca9a529a
User <user@Stanford-N.local>
2017-03-08 14:57:24 -0500

    Removed space

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 function setup() {
-
+createCanvas(800,600);
 }
-
+var xLocation = 100;
 function draw() {
-  
+rect(xLocation ,100,50,50);
+xLocation = xLocation + 1;
+
+if(xLocation  > 50    ){
+  rect(0,0,100,100);
+}
 }
