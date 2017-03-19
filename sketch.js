5eba0c74b05eef3346d7d38747b1302ae6c51a32
User <user@Stanford-N.fios-router.home>
2017-03-19 19:58:25 -0400

    Replaced code with image test

--- sketch.js
+++ sketch.js
@@ -1,12 +1,39 @@
-function setup() {
-createCanvas(800,600);
-}
-var xLocation = 100;
-function draw() {
-rect(xLocation ,100,50,50);
-xLocation = xLocation + 1;
+//Stanford empty project
 
-if(xLocation  > 50    ){
-  rect(0,0,100,100);
+// DEFINE variables here
+var player;
+var myImage;
+function preload(){
+    myImage = loadImage('/smallGuy.png');
+//Load any resources, such as images here
 }
+
+function setup() {
+    createCanvas(800,600)
+    player  = createSprite(300,300,50,102);
+player.addImage(myImage);
+//INSTANTIATE variables here
+
+}
+
+function draw() {
+
+//UPDATE variables here
+drawSprites()
+    if(keyDown(LEFT)){
+//Put what you want to happen when left is pressed in here
+
+}
+
+if(keyDown(RIGHT)){
+//Put what you want to happen when right is pressed in here
+
+}
+
+}
+
+function mouseClicked() {
+
+//Put things you want to happen when you click the mouse in here
+
 }
